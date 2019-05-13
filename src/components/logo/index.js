a191737ed185c3d6a836b079ec83531d778ddd84
Marek Bareła <marek.barela@wp.pl>
2019-05-13 03:29:51 +0200

    Logo commited

---
+++ src/components/logo/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { StaticQuery, graphql } from "gatsby"
+import Img from "gatsby-image";
+import styles from './index.module.css';
+
+const Logo = ({ data }) => {
+  const { wrapper } = styles;
+  const logo = data.logo.childImageSharp.fixed;
+  return (
+    <div className={wrapper}>
+      <Img fixed={logo} />
+    </div>
+  )
+}
+
+
+export default props => (
+  <StaticQuery
+    query={graphql`
+        query {
+          logo: file(relativePath: { eq: "logo.png" }) {
+            childImageSharp {
+              fixed(width: 150) {
+                ...GatsbyImageSharpFixed
+              }
+            }
+          }
+        }
+      `}
+    render={data => <Logo data={data} {...props} />}
+  />
+)
---
+++ src/components/navigation/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navigation = () => {
+  return (
+    <div>
+      navigation
+    </div>
+  )
+}
+
+export default Navigation;
