b1bbc1eec6fee4e2942fc0d20dcf5f400284985e
Marek Bareła <marek.barela@wp.pl>
2019-05-13 04:58:43 +0200

    mobile navigation styling commited

--- src/components/navigation-mobile/index.js
+++ src/components/navigation-mobile/index.js
@@ -1,12 +1,22 @@
 import React from 'react';
+import ListItem from '../navigation-list-item';
 import styles from './index.module.css';
 
 const Mobile = () => {
-  const { list } = styles;
+  const { list, button } = styles;
   return (
-    <ul className={list}>
-
-    </ul>
+    <>
+      <button className={button} aria-label="menu"></button>
+      <ul className={list}>
+        <ListItem to="#Home" text="Home" />
+        <ListItem to="#Services" text="Services" />
+        <ListItem to="#Portfolio" text="Portfolio" />
+        <ListItem to="#About" text="About" />
+        <ListItem to="#Pricing" text="Pricing" />
+        <ListItem to="#Contact" text="Contact" />
+        <ListItem to="#Features" text="Features" />
+      </ul>
+    </>
   )
 }
 
