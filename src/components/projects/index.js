68a6b53646be0e8f2fedc1c24398cc4ce2c04dce
Marek Bareła <marek.barela@wp.pl>
2019-05-14 23:10:19 +0200

    project section header commited

--- src/components/projects/index.js
+++ src/components/projects/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
-
+import SectionHeader from '../section-header';
+import styles from './index.module.css';
 
 const Projects = () => {
+  const { wrapper } = styles;
   return (
-    <section>
-      
+    <section id="Portfolio">
+      <div className={wrapper}>
+        <SectionHeader 
+          headerText="OUR AWESOME PROJECTS"
+          paragraphText="GREAT DESIGN COMES WITH UNDERSTANDING CUSTOMER NEEDS"
+        />
+      </div>
     </section>
   )
 }
