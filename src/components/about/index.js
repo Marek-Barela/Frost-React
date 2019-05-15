216d45ab07725b942e58a172553b83a5505bd019
Marek Bareła <marek.barela@wp.pl>
2019-05-15 18:34:16 +0200

    about header commited

--- src/components/about/index.js
+++ src/components/about/index.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import SectionHeader from '../section-header';
+import styles from './index.module.css';
 
 const About = () => {
+  const { wrapper } = styles;
   return (
     <section id="About">
-      
+      <div className={wrapper}>
+        <SectionHeader
+          headerText="ABOUT THE COMPANY"
+          paragraphText="A CREATIVE AGENCY TURNING IDEAS INTO BEAUTIFUL THINGS"
+        />
+      </div>
     </section>
   )
 }
