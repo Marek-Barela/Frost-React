757276f1b9bdb22a5c1738327c16f7474c4deeb4
Marek Bareła <marek.barela@wp.pl>
2019-05-15 17:58:21 +0200

    smooth scroll commited

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -3,6 +3,10 @@ import PropTypes from "prop-types";
 import Header from "../header";
 import "./layout.css";
 
+if (typeof window !== "undefined") {
+  require("smooth-scroll")('a[href*="#"]')
+}
+
 const Layout = ({ children }) => (
   <>
     <Header />
