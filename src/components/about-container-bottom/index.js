9102960e99f8abc65af563880e0605d40c2de8d0
Marek Bareła <marek.barela@wp.pl>
2019-05-15 20:38:41 +0200

    skills bar functionality commited

--- src/components/about-container-bottom/index.js
+++ src/components/about-container-bottom/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import SkillBar from '../about-skill-bar';
 import styles from './index.module.css';
 
 const ContainerBottom = () => {
@@ -10,6 +11,10 @@ const ContainerBottom = () => {
         <p>
           Duis non lectus sit amet est imperdiet cursus elementum vitae eros. Etiam adipiscingmorbi vitae magna tellus, ac mattis urna phasellus rhoncus.
         </p>
+        <SkillBar name="Photoshop" procentage="90%" />
+        <SkillBar name="Final Cut" procentage="80%" />
+        <SkillBar name="Studio Photography " procentage="85%" />
+        <SkillBar name="Motion Video " procentage="75%" />
       </div>
       <div className={descriptionContainer}>
         <h4>WHY CHOOSE US?</h4>
--- src/components/about-container/index.js
+++ src/components/about-container/index.js
@@ -5,7 +5,6 @@ import styles from './index.module.css';
 
 const AboutContainer = () => {
   const { wrapper } = styles;
-
   return (
     <div className={wrapper}>
       <ContainerTop />
