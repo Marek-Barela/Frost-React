97f2733c0b1ee381788e754a7e0aaaf0b53f7041
Marek Bareła <marek.barela@wp.pl>
2019-05-16 23:42:04 +0200

    pricing functionality commited

--- src/components/pricing-container/index.js
+++ src/components/pricing-container/index.js
@@ -6,10 +6,26 @@ const PricingContainer = () => {
   const { wrapper } = styles;
   return (
     <div className={wrapper}>
-      <PricingItem />
-      <PricingItem />
-      <PricingItem />
-      <PricingItem />
+      <PricingItem
+        name="Bronze"
+        cost="3"
+        time="3 Days"
+      />
+      <PricingItem
+        name="Silver"
+        cost="10"
+        time="7 Days"
+      />
+      <PricingItem
+        name="Gold"
+        cost="20"
+        time="30 Days"
+      />
+      <PricingItem
+        name="Platinum"
+        cost="30"
+        time="120 Days"
+      />
     </div>
   )
 }
--- src/components/pricing-item/index.js
+++ src/components/pricing-item/index.js
@@ -1,13 +1,33 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import styles from './index.module.css';
 
-const PricingItem = () => {
-  const { wrapper } = styles;
+const PricingItem = ({ name, cost, time }) => {
+  const { wrapper, contentWrapper, header, price, list, button } = styles;
   return (
     <div className={wrapper}>
-
+      <div className={contentWrapper}>
+        <h4 className={header}>{name}</h4>
+        <span className={price}>{cost}</span>
+        <ul className={list}>
+          <li>{time}</li>
+          <li>2GB Storage</li>
+          <li>25 Users</li>
+          <li>Unlimited Pages</li>
+          <li>Enhanced Security</li>
+        </ul>
+        <button className={button}>
+          Select Plan
+        </button>
+      </div>
     </div>
   )
+}
+
+PricingItem.propTypes = {
+  name: PropTypes.string.isRequired,
+  cost: PropTypes.string.isRequired,
+  time: PropTypes.string.isRequired
 }
 
 export default PricingItem
