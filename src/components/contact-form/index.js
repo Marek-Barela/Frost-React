980bfc675e7350e74f5ceb08872f1da8791ef298
Marek Bareła <marek.barela@wp.pl>
2019-05-17 22:24:01 +0200

    contact form styling commited

--- src/components/contact-form/index.js
+++ src/components/contact-form/index.js
@@ -2,11 +2,20 @@ import React from 'react';
 import styles from './index.module.css';
 
 const ContactForm = () => {
-  const { wrapper } = styles;
+  const { form, inputContainer, nameInput, emailInput, button } = styles;
   return (
-    <div className={wrapper}>
-
-    </div>
+    <form className={form} noValidate>
+      <div className={inputContainer}>
+        <div className={nameInput}>
+          <input type="text" placeholder="Name" />
+        </div>
+        <div className={emailInput}>
+          <input type="text" placeholder="Email" />
+        </div>
+      </div>
+      <textarea name="" id="" placeholder="Your Message" />
+      <button type="submit" className={button}>Send Message</button>
+    </form>
   )
 }
 
