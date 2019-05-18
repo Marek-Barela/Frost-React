ecc551501c2de0d6a3b782087c354043a82238ff
Marek Bareła <marek.barela@wp.pl>
2019-05-18 21:04:06 +0200

    last uppdates

--- src/components/seo.js
+++ src/components/seo.js
@@ -1,10 +1,3 @@
-/**
- * SEO component that queries for data with
- *  Gatsby's useStaticQuery React hook
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
-
 import React from "react"
 import PropTypes from "prop-types"
 import Helmet from "react-helmet"
@@ -83,9 +76,9 @@ function SEO({ description, lang, meta, keywords, title }) {
 
 SEO.defaultProps = {
   lang: `en`,
-  meta: [],
-  keywords: [],
-  description: ``,
+  meta: ["frost"],
+  keywords: ["frost", "webside"],
+  description: `Grost is a very simple webside`,
 }
 
 SEO.propTypes = {
