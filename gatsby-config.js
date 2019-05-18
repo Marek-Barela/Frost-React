da76cc54a46e9f83f56bfed3efe8a4134046edb9
Marek Bareła <marek.barela@wp.pl>
2019-05-18 20:49:58 +0200

    images optimizied

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,7 +24,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`,
+        icon: `src/images/frost.png`,
       },
     },
     // `gatsby-plugin-offline`,
